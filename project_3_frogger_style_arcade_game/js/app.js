6af43b892d5fc23222a6a1bdce87e961d9418a1a
3noki <WillSeligman@gmail.com>
2018-05-26 13:10:06 -0600

    update player

--- project_3_frogger_style_arcade_game/js/app.js
+++ project_3_frogger_style_arcade_game/js/app.js
@@ -1,6 +1,7 @@
-row = [51, 132, 213, 294, 375];
+row = [51, 132, 213, 294, 375, 456];
 column = [0, 101, 202, 303, 404];
-
+lives = $(".lives");
+score = $(".score");
 
 // Enemies our player must avoid
 var Enemy = function(x,y,speed) {
@@ -18,11 +19,7 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-    // this.x = x;
-    // this.y = y;
-    // this.speed = speed;
     this.x += this.speed * dt;
-
 };
 
 // Draw the enemy on the screen, required method for game
@@ -36,33 +33,47 @@ Enemy.prototype.render = function() {
 
 var Player = function() {
   this.reset();
-  this.lives = 3;
-};
+  this.lives = "3";
+  this.sprite = 'images/char-boy.png';
+}
 
-Player.prototype.catgirl = function() {
-  this.sprite = 'images/char-cat-girl.png';
+Player.prototype.character = function() {
+  this.sprite = 'images/char-boy.png';
 }
-Player.prototype.horngirl = function () {
-  this.sprite = 'images/char-horn-girl.png';
-}
+
+// function character() {
+// Player.prototype.changecharacter = function() {
+//   if ($(".button").id === "#catgirlbutton") {
+//     this.sprite = 'images/char-cat-girl.png';
+//   }
+//   if ($(".button").id === "#horngirlbutton") {
+//     this.sprite = 'images/char-horn-girl.png';
+//     console.log("horn girl");
+//   }
+//   else {
+//   this.sprite = 'images/char-boy.png';
+//   console.log("else");
+// }
+// }};
+//
+// $(".button").on("click", character());
 
 
 Player.prototype.reset = function() {
   this.column = 2;
-  this.row = 4  ;
+  this.row = 4;
   this.x = column[this.column];
   this.y = row[this.row];
 }
 
 Player.prototype.update = function(dt) {
 
-  //this.x += this.speed * dt;
-
 };
 
 Player.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
+
 
 Player.prototype.handleInput = function(keypress) {
     if (keypress === 'left' && this.x > 0) {
@@ -71,20 +82,42 @@ Player.prototype.handleInput = function(keypress) {
     if (keypress === 'right' && this.x < 404) {
       this.x += 101;
     }
-    if (keypress === 'up' && this.y > 51) {
-      this.y -= 101;
+    if (keypress === 'up' && this.y > 0) {
+      this.y -= 81;
     }
     if (keypress === 'down' && this.y < 375) {
-      this.y += 101;
+      this.y += 81;
+    }
+    if (keypress === 'up' && this.y < 51) {
+      this.loseLife();
     }
 
+};
+
+Player.prototype.loseLife = function () {
+  this.reset()
+  this.lives -= 1;
+  lives.text(this.lives);
+  console.log("dead, reset and and if no lives left reset score")
+
+    if (this.lives === 0) {
+    this.newGame();
+    console.log("if no lives left reset score")
+  }
+
+};
+
+Player.prototype.newGame = function () {
+this.score = 0;
+this.lives = 3;
+lives.text(this.lives);
 };
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 var allEnemies = [];
-var player = new Player(303, 101, 101);
+var player = new Player();
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
