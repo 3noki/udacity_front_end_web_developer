cba61df2dc4ad916d3e7bde4241f65ccc15151d4
3noki <WillSeligman@gmail.com>
2018-05-27 11:12:23 -0600

    addded bugs

--- project_3_frogger_style_arcade_game/js/app.js
+++ project_3_frogger_style_arcade_game/js/app.js
@@ -1,16 +1,34 @@
 row = [51, 132, 213, 294, 375, 456];
 column = [0, 101, 202, 303, 404];
-lives = $(".lives");
+
+bugrow = [0, 101, 202, 303, 404];
+bugcolumn = [51, 132, 213];
+var result = [51, 132, 213][Math.floor(Math.random() * 3)]
+
+//lives = $(".lives");
 score = $(".score");
 
+var bugr = bugrow[Math.floor(Math.random()*bugrow.length)];
+var bugc = bugcolumn[Math.floor(Math.random()*bugcolumn.length)];
+var newbugc = bugcolumn[Math.floor(Math.random()*bugcolumn.length)];
+
+Array.prototype.randomElement = function () {
+    return this[Math.floor(Math.random() * this.length)]
+}
+var myRandomElement = bugcolumn.randomElement()
+
+var random = (Math.floor(Math.random() * .3) * 13) + 101;
+
 // Enemies our player must avoid
-var Enemy = function(x,y,speed) {
+var Enemy = function() {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
-
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+    this.x = Math.random() * 313;
+    this.y = result;
+    this.speed = random;
 };
 
 // Update the enemy's position, required method for game
@@ -20,6 +38,10 @@ Enemy.prototype.update = function(dt) {
     // which will ensure the game runs at the same speed for
     // all computers.
     this.x += this.speed * dt;
+    if (this.x > 505) {
+      this.x = -101 - (Math.floor(Math.random()*10)*10+1);
+      this.y = bugc;
+     }
 };
 
 // Draw the enemy on the screen, required method for game
@@ -30,34 +52,33 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+// let hero = 'char-boy';
+// document.getElementById('boybutton').addEventListener('click', function () {
+//     hero = 'char-boy';
+// });
+//
+// document.getElementById('catgirlbutton').addEventListener('click', function () {
+//       hero = 'char-cat-girl';
+// });
+//
+// document.getElementById('horngirlbutton').addEventListener('click', function () {
+//     hero = 'char-horn-girl';
+// });
+// document.getElementById('princessgirlbutton').addEventListener('click', function () {
+//     hero = 'char-princess-girl';
+// });
+//
+// document.getElementById('pinkgirlbutton').addEventListener('click', function () {
+//     hero = 'char-pink-girl';
+// });
+
 
 var Player = function() {
   this.reset();
-  this.lives = "3";
+  //this.lives = "3";
   this.sprite = 'images/char-boy.png';
+  //this.sprite = 'images/' + hero + '.png';
 }
-
-Player.prototype.character = function() {
-  this.sprite = 'images/char-boy.png';
-}
-
-// function character() {
-// Player.prototype.changecharacter = function() {
-//   if ($(".button").id === "#catgirlbutton") {
-//     this.sprite = 'images/char-cat-girl.png';
-//   }
-//   if ($(".button").id === "#horngirlbutton") {
-//     this.sprite = 'images/char-horn-girl.png';
-//     console.log("horn girl");
-//   }
-//   else {
-//   this.sprite = 'images/char-boy.png';
-//   console.log("else");
-// }
-// }};
-//
-// $(".button").on("click", character());
-
 
 Player.prototype.reset = function() {
   this.column = 2;
@@ -73,7 +94,6 @@ Player.prototype.update = function(dt) {
 Player.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
-
 
 Player.prototype.handleInput = function(keypress) {
     if (keypress === 'left' && this.x > 0) {
@@ -93,30 +113,43 @@ Player.prototype.handleInput = function(keypress) {
     }
 
 };
-
+var lives=3;
 Player.prototype.loseLife = function () {
   this.reset()
-  this.lives -= 1;
-  lives.text(this.lives);
-  console.log("dead, reset and and if no lives left reset score")
-
-    if (this.lives === 0) {
-    this.newGame();
-    console.log("if no lives left reset score")
+  if (lives >0) {
+  lives -= 1;
+  $(".lives").text(lives);
+  //console.log("dead, reset, and and if no lives left reset")
+}
+    if (lives === 0) {
+    message();
+    //console.log("if no lives left show losing message")
   }
-
 };
 
-Player.prototype.newGame = function () {
-this.score = 0;
-this.lives = 3;
-lives.text(this.lives);
+function message() {
+    document.getElementById('loseModal').style.display='block'
+    $(loseModal).modal('show');
+}
+
+function newGame() {
 };
+
+
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-var allEnemies = [];
+// var enemyNum = 5;
+
+var bug1 = new Enemy();
+var bug2 = new Enemy();
+var bug3 = new Enemy();
+var allEnemies = [bug1, bug2, bug3];
+// var allEnemies = [];
+// for (var i = 0; i < enemyNum; i++) {
+//     allEnemies.push(new Enemy());
+// };
 var player = new Player();
 
 // This listens for key presses and sends the keys to your
